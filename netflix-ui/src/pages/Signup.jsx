9f16e0fc10d3c416cd314532069b3b1c3bf76df3
Dheeraj Rajput <dheerajrajput1232@gmail.com>
2023-07-07 14:26:06 +0530

    commit

--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -6,64 +6,7 @@ import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/aut
 import { firebaseAuth } from '../utils/firebase-config';
 import { useNavigate } from 'react-router-dom';
 
-const Container = styled.div`
-    position: relative;
-    .content{
-       position:absolute;
-       top:0;
-       left:0;
-       background-color:rgba(0,0,0,0.5);
-       height:100vh;
-       width:100vw;
-       display:grid;
-       grid-templete-rows:15vh 85vh;
-       .body{
-         gap:1rem;
-         .text{
-          gap:1rem;
-          text-align:center;
-          font-size:2rem;
-          h1{
-             padding: 0.25rem;
-          }
-         }
-         .form{
-          display:grid;
-          grid-template-columns:${({showPassword})=>showPassword ? "1fr 1fr" :"2fr 1fr"};
-          width:60%;
-          input { 
-            color:black;
-            border:none;
-            padding:1.5rem;
-            font-size:1.2rem;
-            border:1px solid black;
-            &:focus{
-              outline:none;
-            }
-          }
-          button{
-            padding:0.5rem  1rem;
-            background-color:#e50914;
-            border:none;
-            curser:pointer;
-            color:white;
-            font-weight: bolder;
-            font-size:1.05rem;
-          }
-         }
-         button{
-            padding:0.5rem  1rem;
-            background-color:#e50914;
-            border:none;
-            curser:pointer;
-            color:white;
-            border-radius:0.2rem ;
-            font-weight: bolder;
-            font-size:1.05rem;
-         }
-       } 
-    }
-`;
+
 const Signup = () => {
     const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate();
@@ -119,3 +62,65 @@ const Signup = () => {
 
 
 export default Signup
+
+
+
+
+const Container = styled.div`
+    position: relative;
+    .content{
+       position:absolute;
+       top:0;
+       left:0;
+       background-color:rgba(0,0,0,0.5);
+       height:100vh;
+       width:100vw;
+       display:grid;
+       grid-templete-rows:15vh 85vh;
+       .body{
+         gap:1rem;
+         .text{
+          gap:1rem;
+          text-align:center;
+          font-size:1.8rem;
+          h1{
+             padding: 0 25rem;
+          }
+         }
+         .form{
+          display:grid;
+          grid-template-columns:${({showPassword})=>showPassword ? "1fr 1fr" :"2fr 1fr"};
+          width:60%;
+          input { 
+            color:black;
+            border:none;
+            padding:1.5rem;
+            font-size:1.2rem;
+            border:1px solid black;
+            &:focus{
+              outline:none;
+            }
+          }
+          button{
+            padding:0.5rem  1rem;
+            background-color:#e50914;
+            border:none;
+            curser:pointer;
+            color:white;
+            font-weight: bolder;
+            font-size:1.05rem;
+          }
+         }
+         button{
+            padding:0.5rem  1rem;
+            background-color:#e50914;
+            border:none;
+            curser:pointer;
+            color:white;
+            border-radius:0.2rem ;
+            font-weight: bolder;
+            font-size:1.05rem;
+         }
+       } 
+    }
+`;
